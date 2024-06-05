09ecf660b862ebedc79f993464b595d63367dbe7
NarineHay <naromisho87@gmail.com>
2024-06-05 17:48:27 +0400

    77

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/clear-cache', function () {
+    //   $run = Artisan::call('migrate');
+    // $run = Artisan::call('db:seed --class=UserInfoSeeder');
+    $run = Artisan::call('migrate:fresh --seed');
+
+    //   $run = Artisan::call('make:model Telegram_Workload');
+    // $run = Artisan::call('config:clear');
+    // $run = Artisan::call('cache:clear');
+    // $run = Artisan::call('config:cache');
+    //   $run = Artisan::call('make:command CalculateTheRatingOfThePartiesCron --command=calculate_the_rating_of_the_parties:cron');
+    // $run = Artisan::call('demo:cron');
+
+    return 'FINISHED';
+});
